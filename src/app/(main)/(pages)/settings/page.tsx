47d8bb0126e2f3fd087a2daac2756fafe5c16e1f
Sneha Kumari <siddharth8271skg2@gmail.com>
2024-04-26 02:49:35 +0530

    connections added

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,31 +1,27 @@
-'use client'
-import ProfileForm from '@/components/forms/ProfileForm'
-import React from 'react'
-import ProfilePicture from './_components/ProfilePicture'
-import { currentUser } from '@clerk/nextjs'
-import { db } from '@/lib/db'
 
+import React from 'react'
+
+import { db } from '@/lib/db'
+import { currentUser } from '@clerk/nextjs'
+import ProfilePicture from './_components/ProfilePicture'
+import ProfileForm from '@/components/forms/ProfileForm'
 
 type Props = {}
 
 const Settings = async (props: Props) => {
-  const authUser = await currentUser();
+  const authUser = await currentUser()
   if (!authUser) return null
 
-  const user = await db.user.findUnique({where : {
-    clerkId : authUser.id
-  }})
-
-
-  const removeProfileImage = async () =>{
+  const user = await db.user.findUnique({ where: { clerkId: authUser.id } })
+  const removeProfileImage = async () => {
+    'use server'
     const response = await db.user.update({
-      where :{
-        clerkId : authUser.id,
-  
+      where: {
+        clerkId: authUser.id,
       },
-      data : {
-        profileImage : ""
-      }
+      data: {
+        profileImage: '',
+      },
     })
     return response
   }
@@ -45,42 +41,44 @@ const Settings = async (props: Props) => {
     return response
   }
 
-  const updateUserInfo = async (name : string) => {
-    const response = await db.user.update({
-      where:{
+  const updateUserInfo = async (name: string) => {
+    'use server'
+
+    const updateUser = await db.user.update({
+      where: {
         clerkId: authUser.id,
       },
       data: {
         name,
       },
     })
-    return response
+    return updateUser
   }
 
-    return (
-        <div className="flex flex-col gap-4">
-          <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
-            <span>Settings</span>
-          </h1>
-          <div className="flex flex-col gap-10 p-6">
-            <div>
-              <h2 className="text-2xl font-bold">User Profile</h2>
-              <p className="text-base text-white/50">
-                Add or update your information
-              </p>
-            </div>
-           <ProfilePicture
-            onDelete={removeProfileImage}
-            userImage={user?.profileImage || ''}
-            onUpload={uploadProfileImage}
-           >
-
-           </ProfilePicture>
-
-            <ProfileForm user = {user} onUpdate= {updateUserInfo} />
-          </div>
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
+        <span>Settings</span>
+      </h1>
+      <div className="flex flex-col gap-10 p-6">
+        <div>
+          <h2 className="text-2xl font-bold">User Profile</h2>
+          <p className="text-base text-white/50">
+            Add or update your information
+          </p>
         </div>
-      )
+        <ProfilePicture
+          onDelete={removeProfileImage}
+          userImage={user?.profileImage || ''}
+          onUpload={uploadProfileImage}
+        />
+        <ProfileForm    
+          user={user}
+          onUpdate={updateUserInfo}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Settings
