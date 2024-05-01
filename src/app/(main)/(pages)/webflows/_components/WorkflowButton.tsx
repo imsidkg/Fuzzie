3be5203bbf5fd7764bc907020339ad0154b04c00
Sneha Kumari <siddharth8271skg2@gmail.com>
2024-05-01 23:49:17 +0530

    first commit

---
+++ src/app/(main)/(pages)/webflows/_components/WorkflowButton.tsx
@@ -0,0 +1,12 @@
+'use client'
+ import React from 'react'
+ 
+ type Props = {}
+ 
+ function WorkflowButton({}: Props) {
+   return (
+     <div>WorkflowButton</div>
+   )
+ }
+ 
+ export default WorkflowButton
