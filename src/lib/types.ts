6fae92191c21501785989f4c9077ae986e55c54e
Sneha Kumari <siddharth8271skg2@gmail.com>
2024-05-08 01:19:59 +0530

    editor configured

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -55,3 +55,30 @@ export type EditorNodeType = {
 }
 
 export type EditorNode = EditorNodeType
+
+export type EditorActions =
+  | {
+      type: 'LOAD_DATA'
+      payload: {
+        elements: EditorNode[]
+        edges: {
+          id: string
+          source: string
+          target: string
+        }[]
+      }
+    }
+  | {
+      type: 'UPDATE_NODE'
+      payload: {
+        elements: EditorNode[]
+      }
+    }
+  | { type: 'REDO' }
+  | { type: 'UNDO' }
+  | {
+      type: 'SELECTED_ELEMENT'
+      payload: {
+        element: EditorNode
+      }
+    }
--- src/providers/EditorProvider.tsx
+++ src/providers/EditorProvider.tsx
@@ -1,4 +1,5 @@
-import { EditorNodeType } from "@/lib/types";
+import { EditorActions, EditorNodeType } from "@/lib/types";
+
 import React from "react";
 import {
   Dispatch,
@@ -16,52 +17,145 @@ export type Editor = {
     source: string;
     target: string;
   }[];
-  selectedNode: EditorNodeType
+  selectedNode: EditorNodeType;
 };
 
 export type HistoryState = {
-    history: Editor[]
-    currentIndex: number
-  }
-  
-  export type EditorState = {
-    editor: Editor
-    history: HistoryState
-  }
+  history: Editor[];
+  currentIndex: number;
+};
 
-  const initialEditorState: EditorState['editor'] = {
-    elements: [],
-    selectedNode: {
-      data: {
-        completed: false,
-        current: false,
-        description: '',
-        metadata: {},
-        title: '',
-        type: 'Trigger',
-      },
-      id: '',
-      position: { x: 0, y: 0 },
-      type: 'Trigger',
+export type EditorState = {
+  editor: Editor;
+  history: HistoryState;
+};
+
+const initialEditorState: EditorState["editor"] = {
+  elements: [],
+  selectedNode: {
+    data: {
+      completed: false,
+      current: false,
+      description: "",
+      metadata: {},
+      title: "",
+      type: "Trigger",
     },
-    edges: [],
-  }
-  
-  const initialHistoryState: HistoryState = {
-    history: [initialEditorState],
-    currentIndex: 0,
-  }
-  
-  const initialState: EditorState = {
-    editor: initialEditorState,
-    history: initialHistoryState,
-  }
-  
+    id: "",
+    position: { x: 0, y: 0 },
+    type: "Trigger",
+  },
+  edges: [],
+};
 
-type Props = {};
+const initialHistoryState: HistoryState = {
+  history: [initialEditorState],
+  currentIndex: 0,
+};
+
+const initialState: EditorState = {
+  editor: initialEditorState,
+  history: initialHistoryState,
+};
+
+const editorReducer = (
+  state: EditorState = initialState,
+  action: EditorActions
+): EditorState => {
+  switch (action.type) {
+    case "REDO": {
+      if (state.history.currentIndex < state.history.history.length - 1) {
+        const nextIndex = state.history.currentIndex + 1;
+        const nextEditorState = { ...state.history.history[nextIndex] };
+        const redoState = {
+          ...state,
+          editor: nextEditorState,
+          history: {
+            ...state.history,
+            currentIndex: nextIndex,
+          },
+        };
+        return redoState;
+      }
+      return state;
+    }
+    case "UNDO": {
+      if (state.history.currentIndex > 0) {
+        const prevIndex = state.history.currentIndex - 1;
+        const prevEditorState = { ...state.history.history[prevIndex] };
+        const undoState = {
+          ...state,
+          editor: prevEditorState,
+          history: {
+            ...state.history,
+            currentIndex: prevIndex,
+          },
+        };
+        return undoState;
+      }
+      return state;
+    }
+
+    case "LOAD_DATA":
+      return {
+        ...state,
+        editor: {
+          ...state.editor,
+          elements: action.payload.elements || initialEditorState.elements,
+          edges: action.payload.edges,
+        },
+      };
+    case "SELECTED_ELEMENT":
+      return {
+        ...state,
+        editor: {
+          ...state.editor,
+          selectedNode: action.payload.element,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export type EditorContextData = {
+  previewMode: boolean;
+  setPreviewMode: (previewMode: boolean) => void;
+};
+
+export const EditorContext = createContext<{
+  state: EditorState;
+  dispatch: Dispatch<EditorActions>;
+}>({
+  state: initialState,
+  dispatch: () => undefined,
+});
+
+type Props = {
+    children : React.ReactNode
+};
 
 const EditorProvider = (props: Props) => {
-  return <div>EditorProvider</div>;
-};
+    const [state, dispatch] = useReducer(editorReducer, initialState)
+
+  return (
+    <EditorContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      {props.children}
+    </EditorContext.Provider>
+  )
+}
+
+export const useEditor = () => {
+    const context = useContext(EditorContext)
+    if (!context) {
+      throw new Error('useEditor Hook must be used within the editor Provider')
+    }
+    return context
+}
 
 export default EditorProvider;
